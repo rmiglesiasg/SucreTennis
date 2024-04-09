66be18f442efec7c903fc33018f2846e2a3754be
Ramoniggr <43300579+rmiglesiasg@users.noreply.github.com>
2024-04-09 20:32:30 +0200

    2

--- Assets/Scripts/MoveBall.cs
+++ Assets/Scripts/MoveBall.cs
@@ -9,18 +9,39 @@ public class MoveBall : MonoBehaviour
     float acc = 0.01f;
 
     float vel;
+    int dirX = +1;
+    int dirY = 1;
+
+    float cameraSize;
 
     // Start is called before the first frame update
     void Start()
     {
         ball = this.GetComponent<Rigidbody2D>();
         vel = 1f;
+        cameraSize = GameObject.Find("Main Camera").GetComponent<Camera>().orthographicSize;
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         vel += acc;
-        ball.velocity = new Vector2(-vel, vel);
+        ball.velocity = new Vector2(dirX*vel, dirY*vel);
+
+        if (ball.position.y >= cameraSize - 3f & dirY > 0)
+            dirY *= -1;
+        else if (ball.position.y <= -(cameraSize - 3f) & dirY < 0)
+            dirY *= -1;
+
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.name == "Player")
+            dirX *= -1;
+        else if (collision.collider.name == "Rival")
+            dirX *= -1;
     }
 }
--- Assets/Scripts/MovePlayer.cs
+++ Assets/Scripts/MovePlayer.cs
@@ -9,7 +9,7 @@ public class MovePlayer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.Find("Player").GetComponent<Transform>();
+        player = this.GetComponent<Transform>(); //GameObject.Find("Player").GetComponent<Transform>();
         cameraSize = GameObject.Find("Main Camera").GetComponent<Camera>().orthographicSize;
     }
 
